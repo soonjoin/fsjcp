1b4a15e29095956a5adad5c62a1c21130e8fff92
sujin <sujin@139.com>
2020-08-13 12:57:45 +0800

    Support retrieving an XML document stored on the webserver

--- fsjcp-service/src/main/java/net/cishow/fsjcp/service/controller/FsConfController.java
+++ fsjcp-service/src/main/java/net/cishow/fsjcp/service/controller/FsConfController.java
@@ -3,16 +3,19 @@ package net.cishow.fsjcp.service.controller;
 import net.cishow.fsjcp.common.entity.fsconf.Sofia;
 import net.cishow.fsjcp.common.entity.fsconf.base.SipProfile;
 import net.cishow.fsjcp.common.entity.fsconf.base.SipProfileType;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 @RequestMapping(value = "/fsconf",
         params = {"section=configuration", "tag_name=configuration"},
         produces = "application/xml;charset=UTF-8")
 public class FsConfController {
 
     @RequestMapping(params = {"key_name=name", "key_value=sofia.conf"})
+    @ResponseBody
     public String sofia() {
         Sofia sofia = new Sofia();
 
@@ -20,5 +23,10 @@ public class FsConfController {
         sofia.getProfiles().put("external", external);
 
         return sofia.toString();
+    }
+
+    @RequestMapping(params = {"key_name=name"})
+    public String timezone(@RequestParam("key_value") String key_value) {
+        return "/fsconf/" + key_value;
     }
 }
